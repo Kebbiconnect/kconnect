f7b485ce9ec22807c5d54899620eb5a724bf049a
kebbikpn <kebbi.kpn@gmail.com>
2025-10-20 14:43:13 +0100

    update

--- KPN/settings.py
+++ KPN/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # No default fallback - must be set in environment variables for security
-SECRET_KEY = config('SESSION_SECRET')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
