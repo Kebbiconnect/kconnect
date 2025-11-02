67bebba72c2d46c08cee2a5398c95d9629d8a84f
Kebbiconnect <kpnkebbi@hotmail.com>
2025-11-02 23:15:16 +0100

    Create forms for media upload and editing
    Added forms for uploading and editing media items.

---
+++ media/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import MediaItem
+
+
+class OpinionUploadForm(forms.Form):
+    """Form for uploading multiple media files (opinions) with captions"""
+    caption = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'rows': 3,
+            'class': 'w-full p-3 border rounded dark:bg-gray-700',
+            'placeholder': 'Add a caption for your media post...'
+        }),
+        label='Caption',
+        required=True,
+        max_length=500
+    )
+
+
+class MediaItemEditForm(forms.ModelForm):
+    """Form for editing individual media items"""
+    class Meta:
+        model = MediaItem
+        fields = ['title', 'description', 'media_type']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'w-full p-3 border rounded dark:bg-gray-700'}),
+            'description': forms.Textarea(attrs={'rows': 3, 'class': 'w-full p-3 border rounded dark:bg-gray-700'}),
+            'media_type': forms.Select(attrs={'class': 'w-full p-3 border rounded dark:bg-gray-700'}),
+      }
