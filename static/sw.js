4067f53be381832cef98a74b269cd178e8db923d
Kebbiconnect <kpnkebbi@hotmail.com>
2026-01-27 13:41:35 +0100

    ads

--- static/sw.js
+++ static/sw.js
@@ -78,3 +78,10 @@ self.addEventListener('activate', (event) => {
   );
   self.clients.claim();
 });
+
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10526591
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
